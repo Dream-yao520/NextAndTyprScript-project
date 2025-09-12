ba24da9eb0efcb667c536d194f0207ad3fe31c9c
Dream-yao520 <3387210184@qq.com>
2025-09-12 16:58:13 +0800

    完成爬虫脚本

---
+++ phonegpt/phone-gap/app/api/chat/route.ts
@@ -0,0 +1,46 @@
+import {
+    embed,
+    streamText
+} from 'ai'
+import {
+    createOpenAI
+} from '@ai-sdk/openai'
+import { createClient } from "@supabase/supabase-js"
+
+const supabase = createClient(
+    process.env.SUPABASE_URL ?? "",
+    process.env.SUPABASE_KEY ?? ""
+)
+const openai = createOpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+    baseURL: process.env.OPENAI_API_BASE_URL
+})
+async function generateEmbedding(message: string) {
+    return embed({
+        model: openai.embedding('text-embedding-3-small'),
+        value: message,
+    })
+}
+
+export async function POST(req: Request) {
+    try {
+        const { messages } = await req.json()
+        const lastestMessage = messages.at(-1).content
+        // embedding
+        const { embedding } = await generateEmbedding(lastestMessage)
+        // console.log(embedding)
+        // 相似度计算
+
+    } catch (err) {
+
+    }
+}
+
+async function fecthRelevantContext(embedding: number[]) {
+    const {
+        data,
+        error
+    } = await supabase.rpc("get_relevant_chunks", {
+
+    })
+}
